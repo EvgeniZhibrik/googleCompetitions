1e4b946d06e2298ae4c6a749fe49f6d0271fa7a0
Yauheni Zhybryk <yzhybryk@exadel.com>
2020-03-04 18:50:46 +0300

    parcels started

--- kickstart/2019/round A/Parcels.js
+++ kickstart/2019/round A/Parcels.js
@@ -56,7 +56,9 @@ function runSolution() {
         }
     }
 
-    let maxCoords = [];
+    const ways = {
+    	0: initialZeros
+    };
 
 	let max = 0;
 	const queue = [];
@@ -70,41 +72,53 @@ function runSolution() {
 		if (r > 0 && arr[r - 1][c] < 0) {
 			arr[r - 1][c] = arr[r][c] + 1;
 			queue.push([r - 1, c]);
+
+			if (!ways[arr[r][c] + 1]) {
+				ways[arr[r][c] + 1] = [];
+			}
+			ways[arr[r][c] + 1].push([r - 1, c]);
+
 			if (arr[r][c] + 1 > max) {
 				max = arr[r][c] + 1;
-				maxCoords = [[r - 1, c]];
-			} else if (arr[r][c] + 1 === max) {
-				maxCoords.push([r - 1, c]);
 			}
 		}
 		if (r < R - 1 && arr[r + 1][c] < 0) {
 			arr[r + 1][c] = arr[r][c] + 1;
 			queue.push([r + 1, c]);
+
+			if (!ways[arr[r][c] + 1]) {
+				ways[arr[r][c] + 1] = [];
+			}
+			ways[arr[r][c] + 1].push([r + 1, c]);
+
 			if (arr[r][c] + 1 > max) {
 				max = arr[r][c] + 1;
-				maxCoords = [[r + 1, c]];
-			} else if (arr[r][c] + 1 === max) {
-				maxCoords.push([r + 1, c]);
 			}
 		}
 		if (c > 0 && arr[r][c - 1] < 0) {
 			arr[r][c - 1] = arr[r][c] + 1;
 			queue.push([r, c - 1]);
+
+			if (!ways[arr[r][c] + 1]) {
+				ways[arr[r][c] + 1] = [];
+			}
+			ways[arr[r][c] + 1].push([r, c - 1]);
+
 			if (arr[r][c] + 1 > max) {
 				max = arr[r][c] + 1;
-				maxCoords = [[r, c - 1]];
-			} else if (arr[r][c] + 1 === max) {
-				maxCoords.push([r, c - 1]);
 			}
 		}
 		if (c < C - 1 && arr[r][c + 1] < 0) {
 			arr[r][c + 1] = arr[r][c] + 1;
 			queue.push([r, c + 1]);
+
+			if (!ways[arr[r][c] + 1]) {
+				ways[arr[r][c] + 1] = [];
+			}
+			ways[arr[r][c] + 1].push([r, c + 1]);
+
 			if (arr[r][c] + 1 > max) {
 				max = arr[r][c] + 1;
-				maxCoords = [[r, c + 1]];
-			} else if (arr[r][c] + 1 === max) {
-				maxCoords.push([r, c + 1]);
 			}
 		}
 	}
@@ -115,77 +129,41 @@ function runSolution() {
 		return;
 	}
 
-	const totalMax = max;
-	let record = max;
+	let totalMax = max;
+	let totalMin = -1;
+	let record = Math.floor((totalMax + totalMin) / 2);
 
-	for (let newI = 0; newI < R; newI++) {
-		for (let newJ = 0; newJ < C; newJ++) {
-			if (arr2[newI][newJ] === -1 &&
-				maxCoords.every((val) => {
-					return Math.abs(val[0] - newI) + Math.abs(val[1] - newJ) < record;
-				})) {
-				for (let i = 0; i < R; i++) {
-					arr[i] = [];
-					for (let j = 0; j < C; j++) {
-						arr[i][j] = arr2[i][j];
+	while (totalMax - totalMin > 1) {
+		let flag = false;
+		for (let newI = 0; newI < R && !flag; newI++) {
+			for (let newJ = 0; newJ < C && !flag; newJ++) {
+				if (arr2[newI][newJ] === -1) {
+					let newFlag = true;
+					for (let len = max; len > record; len--) {
+						if (!ways[len].every((val) => {
+							return Math.abs(val[0] - newI) + Math.abs(val[1] - newJ) <= record;
+						})) {
+							newFlag = false;
+							break;
+						}
+					}
+
+					if (newFlag) {
+						flag = true;
 					}
 				}
-				arr[newI][newJ] = 0;
-
-				max = 0;
-				count = initialNumber + 1;
-
-				max = fillArray(arr, initialZeros.concat([[newI, newJ]]));
-
-				if (record > max) {
-					record = max;
-				}
 			}
 		}
+
+		if (flag) {
+			totalMax = record;
+		} else {
+			totalMin = record;
+		}
+
+		record = Math.floor((totalMax + totalMin) / 2);
 	}
 
-    console.log('Case #' + (test + 1) + ': ' + record);
+    console.log('Case #' + (test + 1) + ': ' + totalMax);
     test++;
-}
-
-function fillArray(arr, zeros) {
-	let max = 0;
-	const queue = [];
-	for (let i = 0; i < zeros.length; i++) {
-		const [r, c] = zeros[i];
-		queue.push([r, c]);
-	}
-
-	while (queue.length) {
-		const [r, c] = queue.shift();
-		if (r > 0 && arr[r - 1][c] < 0) {
-			arr[r - 1][c] = arr[r][c] + 1;
-			queue.push([r - 1, c]);
-			if (arr[r][c] + 1 > max) {
-				max = arr[r][c] + 1;
-			}
-		}
-		if (r < R - 1 && arr[r + 1][c] < 0) {
-			arr[r + 1][c] = arr[r][c] + 1;
-			queue.push([r + 1, c]);
-			if (arr[r][c] + 1 > max) {
-				max = arr[r][c] + 1;
-			}
-		}
-		if (c > 0 && arr[r][c - 1] < 0) {
-			arr[r][c - 1] = arr[r][c] + 1;
-			queue.push([r, c - 1]);
-			if (arr[r][c] + 1 > max) {
-				max = arr[r][c] + 1;
-			}
-		}
-		if (c < C - 1 && arr[r][c + 1] < 0) {
-			arr[r][c + 1] = arr[r][c] + 1;
-			queue.push([r, c + 1]);
-			if (arr[r][c] + 1 > max) {
-				max = arr[r][c] + 1;
-			}
-		}
-	}
-	return max;
 }
